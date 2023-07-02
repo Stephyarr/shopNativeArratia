b85865d2276ca62c152c532acc8f15f695f12756
Stephyarr <stephaniearratia21@gmail.com>
2023-07-01 23:59:15 -0300

    initial-fonts

---
+++ src/index.jsx
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native';
+import { styles } from "./styles";
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+    </View>
+  );
+}
+
+export default App;
